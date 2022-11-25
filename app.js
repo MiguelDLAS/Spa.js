1d62e7f33cf97775f5b0551c6668127c676d65bf
MIGUEL <mikiserrano2018@gmail.com>
2022-11-25 12:14:23 -0600

    dowload  bulma.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const mainContent = document.querySelector('#container');
+const navbar = document.querySelector('.navbar');
 
+const loadData = e => {
+    e.preventDefault();
+    if(e.target.classList.contains('') || e.target.classList.contains('')){
+        
+    }
+}
 window.addEventListener('   DOMcontentLoaded',()=>{
     fetch('../page/home.html')
     .then(page => {
-        const div = document.createElement('div');
-        console.log(page);
+        return page.text();
+
     })
     .then(contentHtml => {
         const div = document.createElement('div');
+        div
         console.log(contentHtml);
             mainContent.appendChild(div);
     })
+    navbar.addEventListener('click', loadData);
 })
